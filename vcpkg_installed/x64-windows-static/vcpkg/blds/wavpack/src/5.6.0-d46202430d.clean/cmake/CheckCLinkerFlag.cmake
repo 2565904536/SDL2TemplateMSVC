79a7b873d9430fad2599ceaf41ef35929f79498d
iLan <93776632+2565904536@users.noreply.github.com>
2024-07-06 19:24:12 +0800

    Delete vcpkg_installed directory

--- vcpkg_installed/x64-windows-static/vcpkg/blds/wavpack/src/5.6.0-d46202430d.clean/cmake/CheckCLinkerFlag.cmake
+++
@@ -1,9 +0,0 @@
-include(CMakePushCheckState)
-include(CheckCSourceCompiles)
-
-macro(CHECK_C_LINKER_FLAG _FLAG _RESULT)
-    cmake_push_check_state(RESET)
-    set(CMAKE_REQUIRED_FLAGS "${_FLAG}")
-    check_c_source_compiles("int main() { return 0;}" ${_RESULT})
-    cmake_pop_check_state()
-endmacro(CHECK_C_LINKER_FLAG)
